44248a01edea46754d0a5b3536022458b9b154b9
Stresia <87347115+Vititpoom@users.noreply.github.com>
2024-07-12 10:07:37 +0700

    authen portrait scrollview

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -44,13 +44,13 @@ const RESOURCES = {"assets/AssetManifest.bin": "0e4856066101012c471ea832fc87940b
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.ico": "538e00fff9daf7322729cb0714068981",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "59b5e4fcf8a3d4b20d65fc02190584dc",
+"flutter_bootstrap.js": "02b885d2a678576ed1fb77e4424b6798",
 "icons/android-chrome-192x192.png": "092468ae79a1f176569f09872d533e09",
 "icons/android-chrome-512x512.png": "697f70407cf1b3d8139758cec5ded7b7",
 "icons/apple-touch-icon.png": "aad69339f8038efaf3771c4639ddd3fc",
-"index.html": "3f37dff579951b6a59d4e9d09a2cc68b",
-"/": "3f37dff579951b6a59d4e9d09a2cc68b",
-"main.dart.js": "cfcba35633543b78d23778ee6341775e",
+"index.html": "7204f22ec4f7bfb61bba3d18fdcee256",
+"/": "7204f22ec4f7bfb61bba3d18fdcee256",
+"main.dart.js": "a77d384d2760233e9de0b78937811788",
 "manifest.json": "8d3be58572398181bdc2cb7094e8dd4b",
 "version.json": "92ab5b480774132d501453dc99721a55"};
 // The application shell files that are downloaded before a service worker can
