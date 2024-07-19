9d644d70976e3fe07084d04b2495bef4bffd03c7
Stresia <87347115+Vititpoom@users.noreply.github.com>
2024-07-19 09:12:25 +0700

    update 7.19.24(Marketplace)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1999439728"
+    serviceWorkerVersion: "3527546366"
   }
 });
