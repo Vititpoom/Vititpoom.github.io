a3f29cf3544c75355ca5445592d93bd0731b4a40
Stresia <87347115+Vititpoom@users.noreply.github.com>
2024-07-23 10:38:16 +0700

    -8-

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "505357053"
+    serviceWorkerVersion: "1502095213"
   }
 });
